f8e8b16712b043bd7d8a0481bbbd137031d1f0ee
adminSxs <934295740@qq.com>
2016-06-01 18:15:42 +0800

    hiuhkjh

--- src/main/java/com/cdtu/wechat/web/controller/NewsController.java
+++ src/main/java/com/cdtu/wechat/web/controller/NewsController.java
@@ -1,5 +1,10 @@
 package com.cdtu.wechat.web.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="news")
 public class NewsController {
 
 }
